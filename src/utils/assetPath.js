0c4cd546e80a011a5d8195c260cb3517dbf9c6ee
doriskao20000619-prog <doris.kao20000619@gmail.com>
2026-06-30 23:42:52 +0800

    Deploy static site

--- src/utils/assetPath.js
+++
@@ -1,7 +0,0 @@
-const publicBase = import.meta.env.BASE_URL || '/';
-
-export function assetPath(path = '') {
-  const value = String(path);
-  if (!value || /^(https?:|mailto:|data:|blob:)/.test(value)) return value;
-  return `${publicBase}${value.replace(/^\/+/, '')}`;
-}
